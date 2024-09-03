5899c106a7c73ba0d7c2429ee996507181a31060
RawPineapple <95847271+RawPineapple@users.noreply.github.com>
2024-09-03 21:55:19 +0800

    Update and rename crack.js to RevnueCat.js

--- RevnueCat.js
+++ RevnueCat.js
@@ -3,11 +3,10 @@
 
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/RawPineapple/RuleSet/main/crack.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/ddgksf2013/Scripts/master/deleteHeader.js
-^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|\w{9}) url script-echo-response https://github.com/ddgksf2013/dev/raw/main/flightradar24.js
 
 [mitm]
 
-hostname=api.revenuecat.com,mobile.flightradar24.com
+hostname=api.revenuecat.com
 
 ***********************************/
 
