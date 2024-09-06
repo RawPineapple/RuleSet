5ca4eee94d6830a6bee44784771cbfe9b5dae405
RawPineapple <95847271+RawPineapple@users.noreply.github.com>
2024-09-06 17:01:18 +0800

    Update RevnueCat.js

--- RevnueCat.js
+++ RevnueCat.js
@@ -1,7 +1,7 @@
 /***********************************
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/RawPineapple/RuleSet/main/RevenueCat.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/RawPineapple/RuleSet/main/RevnueCat.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/ddgksf2013/Scripts/master/deleteHeader.js
 
 [mitm]
