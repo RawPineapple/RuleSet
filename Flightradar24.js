01a5f47d531c2be7260817cdb100d3de43a7bacd
RawPineapple <95847271+RawPineapple@users.noreply.github.com>
2024-09-08 16:25:51 +0800

    Update Flightradar24.js

--- Flightradar24.js
+++ Flightradar24.js
@@ -1,5 +1,9 @@
-[mitm]
-hostname = mobile.flightradar24.com
+#!name=Flightradar24
+#!desc=破解Flightradar24会员
+#!category=Unlock
 
-[rewrite_local]
-^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|\w{9}) url script-echo-response https://github.com/ddgksf2013/dev/raw/main/flightradar24.js
+[Script]
+flightradar24 = type=http-request, pattern=^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|\w{9}), script-path=https://github.com/ddgksf2013/dev/raw/main/flightradar24.js, requires-body=true, timeout=60
+
+[MITM]
+hostname = %APPEND% mobile.flightradar24.com
